7ecc19c435d56f021889b89447f452e6de497937
Gleb <demonxklimenko@mail.ru>
2021-07-15 04:56:02 +0300

    Update

--- src/test/java/ru/netology/generator/DataGenerator.java
+++ src/test/java/ru/netology/generator/DataGenerator.java
@@ -7,33 +7,39 @@ import java.util.Locale;
 import java.util.Random;
 
     public class DataGenerator {
-    static Faker faker = new Faker(new Locale("ru"));
+        static Faker faker = new Faker(new Locale("ru"));
 
-    private DataGenerator() {
+        private DataGenerator() {
+        }
+
+        public static String cityForInput() {
+            Random random = new Random();
+            int rand = random.nextInt(12);
+            String city[] = {"Екатеринбург", "Москва", "Калининград", "Кемерово", "Тверь", "Кострома", "Краснодар",
+                    "Красноярск", "Рязан", "Курск", "Санкт-Петербург", "Чебоксары"};
+            return city[rand];
+        }
+
+        public static String dataPhone() {
+            String phone = faker.phoneNumber().phoneNumber();
+            return phone;
+        }
+
+        public static String dataName() {
+            String name = faker.name().lastName();
+            name = name + " " + faker.name().firstName();
+            return name;
+        }
+
+        public static String dataInput(int days) {
+            String inputDate = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
+            return inputDate;
+        }
+
+        public static String cityNoVal() {
+            Random random = new Random();
+            int rand = random.nextInt(8);
+            String cityNoVal[] = {"Мегион", "Кизляр", "Урус-Мартан", "Снежинск", "Кингисепп", "Заринск", "Курганинск"};
+            return cityNoVal[rand];
+        }
     }
-
-    public static String cityForInput() {
-        Random random = new Random();
-        int rand = random.nextInt(12);
-        String city[] = {"Екатеринбург", "Москва", "Калининград", "Кемерово", "Тверь", "Кострома", "Краснодар",
-                "Красноярск", "Разян", "Курск", "Санкт-Петербург", "Чебоксары"};
-        return city[rand];
-    }
-
-    public static String dataPhone() {
-        String phone = faker.phoneNumber().phoneNumber();
-        return phone;
-    }
-
-    public static String dataName() {
-        String name = faker.name().lastName();
-        name = name + " " + faker.name().firstName();
-        return name;
-    }
-
-    public static String dataInput(int days) {
-        String inputDate = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
-        return inputDate;
-    }
-
-}
