22eee3414e3360f8a6958348788e89b98a0c4bdd
Gleb <demonxklimenko@mail.ru>
2021-07-19 00:45:47 +0300

    Fix DataGenerator

--- src/test/java/ru/netology/generator/DataGenerator.java
+++ src/test/java/ru/netology/generator/DataGenerator.java
@@ -8,15 +8,16 @@ import java.util.Random;
 
     public class DataGenerator {
         static Faker faker = new Faker(new Locale("ru"));
+        private static String [] city = {"Екатеринбург", "Москва", "Калининград", "Кемерово", "Тверь", "Кострома", "Краснодар",
+                "Красноярск", "Курск", "Санкт-Петербург", "Чебоксары"};
+        private static String [] cityNoVal = {"Мегион", "Кизляр", "Урус-Мартан", "Снежинск", "Кингисепп", "Заринск", "Курганинск"};
 
         private DataGenerator() {
         }
 
         public static String cityForInput() {
             Random random = new Random();
-            int rand = random.nextInt(11);
-            String city [] = {"Екатеринбург", "Москва", "Калининград", "Кемерово", "Тверь", "Кострома", "Краснодар",
-                    "Красноярск", "Курск", "Санкт-Петербург", "Чебоксары"};
+            int rand = random.nextInt(city.length);
             return city[rand];
         }
 
@@ -38,8 +39,7 @@ import java.util.Random;
 
         public static String cityNoVal() {
             Random random = new Random();
-            int rand = random.nextInt(7);
-            String cityNoVal[] = {"Мегион", "Кизляр", "Урус-Мартан", "Снежинск", "Кингисепп", "Заринск", "Курганинск"};
+            int rand = random.nextInt(cityNoVal.length);
             return cityNoVal[rand];
         }
     }
